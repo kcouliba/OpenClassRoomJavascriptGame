be30cf00302ba19a49a5c215d0637be80e1fd37c
kcouliba <coulibaly.d.kevin@gmail.com>
2015-10-23 05:33:29 +0200

    Created / Implemented Grid Object and function generateGrid

---
+++ js/grid.js
@@ -0,0 +1,100 @@
+/*
+** grid.js
+** Author coulibaly.d.kevin@gmail.com
+** Date 23/10/2015
+** Grid definition
+*/
+
+/** Constants values **/
+const SIZE = 10;
+const MAX_OBSTACLE_COUNT = (SIZE * SIZE) * .1;
+const FREE = 0; // Free place on the grid
+
+/*
+** Grid Class
+*/
+var Grid = {
+    size: SIZE,
+    obstacles: Math.floor(Math.random() * MAX_OBSTACLE_COUNT), // Count of obstacles on the grid
+    grid: [],
+    
+    /*
+    ** init
+    ** Constructor
+    */
+    init: function() {
+        this.grid = generateGrid(this.size, this.obstacles);
+        if (DEBUG) {
+            console.log("New grid created.");
+        }
+    },
+    
+    /*
+    ** toString
+    ** Returns a string representation of the instance
+    ** @return String
+    */
+    toString: function() {
+        var grid = "";
+        
+        for (var i = 0; i < this.size; i++) {
+            var line = "";
+            for (var j = 0; j < this.size; j++) {
+                line += this.grid[i * this.size + j]
+            }
+            line.trim();
+            grid += line + "\n";
+        }
+        return (grid);
+    }
+};
+
+/*
+** generateGrid
+** Randomly generates a grid
+** @param size : Number
+** @param obstacleCount : Number
+** @return Array
+*/
+function generateGrid(size, obstacleCount) {
+    var cellsCount = size * size;
+    var row = 0;
+    var grid = [];
+    
+    for (var i = 0; i < cellsCount; i++) {
+        row += (i % 10 == 0) ? 1 : 0;
+        if ((Math.round(Math.random() * 100) < 15) && (obstacleCount-- > 0)) {
+            grid.push(1);
+        } else {
+            grid.push(0);
+        }
+    }
+    if (DEBUG) {
+        console.log("New grid randomly generated.");
+    }
+    return (grid);
+}
+
+/** Tests **/
+
+var grid = Object.create(Grid);
+
+grid.init();
+
+/** Tests End **/
+
+/*
+** Grid Class
+** Alternative version
+*/
+/*
+function Grid() {
+    this.size = SIZE;
+    this.obstacles = Math.floor(Math.random() * MAX_OBSTACLE_COUNT);
+    this.grid = [];
+    
+    this.init = function() {
+        
+    };
+}
+*/
