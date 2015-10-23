469cd9cd76063b0acd30f053cf58598e20c127a5
kcouliba <coulibaly.d.kevin@gmail.com>
2015-10-23 06:12:49 +0200

    Basic scenario editing for automatisation

--- js/tests.js
+++ js/tests.js
@@ -23,13 +23,27 @@ var chain = Object.create(Weapon);
 var p1 = Object.create(Player);
 var p2 = Object.create(Player);
 
-
+// Grid initializing
 grid.init();
 
+// Weapon initializing
 gloves.init("Gloves", Math.round(DEFAULT_DAMAGE));
 baseballBat.init("Baseball Bat", Math.round(DEFAULT_DAMAGE * 1.5));
 chain.init("Chain", Math.round(DEFAULT_DAMAGE * 1.8));
 knife.init("Knife", Math.round(DEFAULT_DAMAGE * 2));
 
+// Player initializing
 p1.init(null, true);
 p2.init(null, false);
+
+// Equip weapon
+p1.equipWeapon(baseballBat);
+p2.equipWeapon(knife);
+
+// Action selecting
+p1.setStance(ATTACK_STANCE);
+p2.setStance(ATTACK_STANCE);
+
+// Action between players
+p1.attack(p2);
+p2.attack(p1);
