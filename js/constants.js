4a6fb317647505d04e02c5431e8207e1903eb29d
kcouliba <coulibaly.d.kevin@gmail.com>
2015-10-29 03:47:27 +0100

    created a constant files for the app

---
+++ js/constants.js
@@ -0,0 +1,20 @@
+const DEBUG = true;
+
+/*
+** Grid constants
+*/
+const SIZE = 10;
+
+/*
+** Player constants
+*/
+const DEFAULT_PLAYER_NAME = "Player";
+const MAX_PLAYER_COUNT = 2;
+const MAX_HP = 100;
+const MAX_MOVE = 3; // How many cells can the player go through
+
+/*
+** Weapon constants
+*/
+const DEFAULT_WEAPON_NAME = "Gloves";
+const DEFAULT_DAMAGE = 10;
